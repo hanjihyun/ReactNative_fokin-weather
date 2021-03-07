395f5496f1986c7e41fddaa44c747521edd9897f
한지현 <gksgpals96@naver.com>
2021-03-07 16:53:24 +0900

    Feat: flex 예시

--- App.js
+++ App.js
@@ -4,18 +4,38 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // <View>는<div>와 비슷
+    <View style={styles.container}> 
+      {/* <Text style={styles.text}>Hello~</Text>
+      <Text style={styles.text}>Hello~</Text> */}
+      <View style={styles.yellowView}>
+        <Text style={styles.text}></Text>
+      </View>
+      <View style={styles.blueView}>
+       <Text style={styles.text}></Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  // flex 잘 활용 핸드폰 크기 달라도 맞춰질꺼야
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    // flexDirection:'row', //줄바꾸기 안함
+    // alignItems: 'center',
     justifyContent: 'center',
   },
+  // text: {
+  //   color: 'black',
+  // },
+  yellowView: {
+    flex: 1,
+    backgroundColor:'yellow'
+  },
+  blueView: {
+    flex: 2,
+    backgroundColor:'blue'
+  }
 });
